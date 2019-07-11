759d43004105fb783fdb5d685900364c3782667f
bingo <bingovhh@163.com>
2019-07-11 11:47:07 +0800

    把郭大师加回来了

--- src/main/java/com/bingo/oalert/util/PersonConst.java
+++ src/main/java/com/bingo/oalert/util/PersonConst.java
@@ -14,6 +14,7 @@ public class PersonConst {
 
     public static final String ZHANGZHB_EMPID = "716e7770-1470-4561-8e61-6cbff4e297d3";
     public static final String WANGLX_EMPID = "c24a2636-9fbc-498c-8ee4-9eb00e9cbb44";
+    public static final String GUOXX_EMPID = "c97a9b96-db7f-43e5-a9c3-464972c65e83";
     public static final String GAOTY_EMPID = "2769fa48-87ba-41e1-83e9-c41350f62e6d";
     public static final String LILONG_EMPID = "b02a2e69-bce6-4ea8-ac5c-b9260b79007d";
 
@@ -30,5 +31,8 @@ public class PersonConst {
         //李龙
         PERSOPN_LIST.add(LILONG_EMPID);
         PHONE_MAP.put(LILONG_EMPID, "17865164412");
+        //郭鑫鑫
+        PERSOPN_LIST.add(GUOXX_EMPID);
+        PHONE_MAP.put(GUOXX_EMPID, "15589779987");
     }
 }
